99e2b8c55a1cf0454ee2338a1ecf83674f4bda57
Anshu Yadav <103017387+king-ronin04@users.noreply.github.com>
2023-08-06 20:30:28 +0530

    Update sudoku.cpp

--- sudoku.cpp
+++ sudoku.cpp
@@ -12,27 +12,26 @@ int grid[N][N] = {
    {0, 0, 0, 0, 0, 0, 0, 7, 4},
    {0, 0, 5, 2, 0, 6, 3, 0, 0}
 };
-bool isPresentInCol(int col, int num){ //check whether num is present in col or not
+bool isPresentInCol(int col, int num){
    for (int row = 0; row < N; row++)
       if (grid[row][col] == num)
          return true;
    return false;
 }
-bool isPresentInRow(int row, int num){ //check whether num is present in row or not
+bool isPresentInRow(int row, int num){
    for (int col = 0; col < N; col++)
       if (grid[row][col] == num)
          return true;
    return false;
 }
 bool isPresentInBox(int boxStartRow, int boxStartCol, int num){
-//check whether num is present in 3x3 box or not
    for (int row = 0; row < 3; row++)
       for (int col = 0; col < 3; col++)
          if (grid[row+boxStartRow][col+boxStartCol] == num)
             return true;
    return false;
 }
-void sudokuGrid(){ //print the sudoku grid after solve
+void sudokuGrid(){
    for (int row = 0; row < N; row++){
       for (int col = 0; col < N; col++){
          if(col == 3 || col == 6)
@@ -47,28 +46,27 @@ void sudokuGrid(){ //print the sudoku grid after solve
       cout << endl;
    }
 }
-bool findEmptyPlace(int &row, int &col){ //get empty location and update row and column
+bool findEmptyPlace(int &row, int &col){
    for (row = 0; row < N; row++)
       for (col = 0; col < N; col++)
-         if (grid[row][col] == 0) //marked with 0 is empty
+         if (grid[row][col] == 0) 
             return true;
    return false;
 }
 bool isValidPlace(int row, int col, int num){
-   //when item not found in col, row and current 3x3 box
    return !isPresentInRow(row, num) && !isPresentInCol(col, num) && !isPresentInBox(row - row%3 ,
 col - col%3, num);
 }
 bool solveSudoku(){
    int row, col;
    if (!findEmptyPlace(row, col))
-      return true; //when all places are filled
-   for (int num = 1; num <= 9; num++){ //valid numbers are 1 - 9
-      if (isValidPlace(row, col, num)){ //check validation, if yes, put the number in the grid
+      return true;
+   for (int num = 1; num <= 9; num++){ 
+      if (isValidPlace(row, col, num)){ 
          grid[row][col] = num;
-         if (solveSudoku()) //recursively go for other rooms in the grid
+         if (solveSudoku()) 
             return true;
-         grid[row][col] = 0; //turn to unassigned space when conditions are not satisfied
+         grid[row][col] = 0; 
       }
    }
    return false;
@@ -78,4 +76,5 @@ int main(){
       sudokuGrid();
    else
       cout << "No solution exists";
+    return 0;
 }
